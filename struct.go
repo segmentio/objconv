e374bfd62e52eb20b643dfa64d5ed4d443df2437
Achille Roussel <achille@segment.com>
2016-12-21 15:11:46 -0800

    [skip ci] merge field.go into struct.go

--- struct.go
+++ struct.go
@@ -5,6 +5,58 @@ import (
 	"sync"
 )
 
+// StructField represents a single field of a struct and carries information
+// useful to the algorithms of the objconv package.
+type StructField struct {
+	// The index of the field in the structure.
+	Index []int
+
+	// The name of the field in the structure.
+	Name string
+
+	// Omitempty is set to true when the field should be omitted if it has an
+	// empty value.
+	Omitempty bool
+
+	// Omitzero is set to true when the field should be omitted if it has a zero
+	// value.
+	Omitzero bool
+
+	// cache for the encoder and decoder methods
+	encode encodeFunc
+	decode decodeFunc
+}
+
+func makeStructField(f reflect.StructField, c map[reflect.Type]*Struct) StructField {
+	t := ParseTag(f.Tag.Get("objconv"))
+	s := StructField{
+		Index:     f.Index,
+		Name:      f.Name,
+		Omitempty: t.Omitempty,
+		Omitzero:  t.Omitzero,
+
+		encode: makeEncodeFunc(f.Type, encodeFuncOpts{
+			recurse: true,
+			structs: c,
+		}),
+
+		decode: makeDecodeFunc(f.Type, decodeFuncOpts{
+			recurse: true,
+			structs: c,
+		}),
+	}
+
+	if len(t.Name) != 0 {
+		s.Name = t.Name
+	}
+
+	return s
+}
+
+func (f *StructField) omit(v reflect.Value) bool {
+	return (f.Omitempty && isEmptyValue(v)) || (f.Omitzero && isZeroValue(v))
+}
+
 // Struct is used to represent a Go structure in internal data structures that
 // cache meta information to make field lookups faster and avoid having to use
 // reflection to lookup the same type information over and over again.
