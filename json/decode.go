3e287b8bd75484d2cbb0b4c7106d99825bcf437d
Achille Roussel <achille@segment.com>
2016-12-20 17:12:53 -0800

    optimize json.Unmarshal

--- json/decode.go
+++ json/decode.go
@@ -15,7 +15,10 @@ func NewDecoder(r io.Reader) *objconv.Decoder {
 }
 
 func Unmarshal(b []byte, v interface{}) (err error) {
-	r := bytes.NewReader(b)
+	// Get a reader from the pool, this saves a memory allocation because Go
+	// fails to realize that the reader doesn't escape.
+	r := readerPool.Get().(*bytes.Reader)
+	r.Reset(b)
 
 	// Get a parser from the pool, this saves a memory allocation because Go
 	// fails to realize that the parser doesn't escape.
@@ -24,8 +27,16 @@ func Unmarshal(b []byte, v interface{}) (err error) {
 
 	err = (objconv.Decoder{Parser: p}).Decode(v)
 
+	p.Reset(nil)
 	parserPool.Put(p)
+
+	r.Reset(nil)
+	readerPool.Put(r)
 	return
+}
+
+var readerPool = sync.Pool{
+	New: func() interface{} { return bytes.NewReader(nil) },
 }
 
 var parserPool = sync.Pool{
