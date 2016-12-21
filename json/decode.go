3f580db3d29970da51fc91dce0c8ec5fcac4b9e4
Achille Roussel <achille@segment.com>
2016-12-20 18:00:28 -0800

    [skip CI] add comments

--- json/decode.go
+++ json/decode.go
@@ -8,12 +8,14 @@ import (
 	"github.com/segmentio/objconv"
 )
 
+// NewDecoder returns a new JSON deocder that parses values from r.
 func NewDecoder(r io.Reader) *objconv.Decoder {
 	return &objconv.Decoder{
 		Parser: NewParser(r),
 	}
 }
 
+// Unmarshal decodes a JSON representation of v from b.
 func Unmarshal(b []byte, v interface{}) (err error) {
 	// Get a reader from the pool, this saves a memory allocation because Go
 	// fails to realize that the reader doesn't escape.
