3f08413f685df0f0675c99859e6d45b828dcc2c6
Achille Roussel <achille@segment.com>
2017-01-02 10:21:08 -0800

    remove mimetype package

--- mimetype/doc.go
+++
@@ -1,8 +0,0 @@
-// Package mimetype provides a global registry that maps mime types to encoders
-// and decoders.
-//
-// Packages providing implementation of parsers and emitters for specific
-// formats should register their codecs to the mimetype package within an init
-// function, so programs loading these packages also get the ability to use the
-// mimetype package as a side effect.
-package mimetype
