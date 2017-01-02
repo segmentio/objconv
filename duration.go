8512568e565fdd865cae0d753dfca736d0ac7c85
Achille Roussel <achille@segment.com>
2017-01-02 10:27:30 -0800

    [skip ci] fix docs

--- duration.go
+++ duration.go
@@ -1,3 +1,5 @@
+package objconv
+
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
@@ -6,7 +8,6 @@
 //
 // The calendrical calculations always assume a Gregorian calendar, with
 // no leap seconds.
-package objconv
 
 import "time"
 
