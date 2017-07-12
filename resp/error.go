942c90ce6416e228568fd5684e1337ea731a8405
Achille Roussel <achille@segment.com>
2017-07-11 19:49:57 -0700

    add resp.Error.Type

--- resp/error.go
+++ resp/error.go
@@ -1,5 +1,10 @@
 package resp
 
+import (
+	"strings"
+	"unicode"
+)
+
 // The Error type represents redis errors.
 type Error string
 
@@ -12,4 +17,25 @@ func NewError(s string) *Error {
 // Error satsifies the error interface.
 func (e *Error) Error() string {
 	return string(*e)
+}
+
+// Type returns the RESP error type, which is represented by the leading
+// uppercase word in the error string.
+func (e *Error) Type() string {
+	s := e.Error()
+
+	if i := strings.IndexByte(s, ' '); i < 0 {
+		s = ""
+	} else {
+		s = s[:i]
+
+		for _, c := range s {
+			if !unicode.IsUpper(c) {
+				s = ""
+				break
+			}
+		}
+	}
+
+	return s
 }
